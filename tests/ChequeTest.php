a064ed49033be8dab49e5bdc45d0e77a3d02e648
Ben McKenzie <ben@ocular.nz>
2025-07-13 22:56:02 +1200

    fix missing namespaces in test

--- tests/ChequeTest.php
+++ tests/ChequeTest.php
@@ -1,15 +1,22 @@
 <?php
 
-class ChequeTest extends SapphireTest {
-  
-  function setUp() {
-    parent::setUp();
-  }
-  
-  public function testClassConfig() {
-		$processor = PaymentFactory::factory('Cheque');
-		$this->assertEquals(get_class($processor), 'ChequeProcessor');
-		$this->assertEquals(get_class($processor->gateway), 'ChequeGateway');
-		$this->assertEquals(get_class($processor->payment), 'Payment');
-	}
+use Payment\PaymentFactory;
+use SilverStripe\Dev\SapphireTest;
+
+class ChequeTest extends SapphireTest
+{
+
+    function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testClassConfig()
+    {
+        $processor = PaymentFactory::factory('Cheque');
+        $this->assertEquals(get_class($processor), 'ChequeProcessor');
+        $this->assertEquals(get_class($processor->gateway), 'ChequeGateway');
+        $this->assertEquals(get_class($processor->payment), 'Payment');
+    }
 }
+
